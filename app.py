bef67101b994747e251aa97e21e80995787a2eee
Bruno Oliveira <brunoal.flat@gmail.com>
2022-09-29 15:37:21 -0300

    Versão teste

---
+++ app.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+
+pyautogui.openbrowser('https://www.eventim.com.br/artist/coldplay-0/coldplay-rio-de-janeiro-3117750/')
+
+
+
+print(f'Hellow princes!')
+
+pyautogui.alert('TEM INGRESSO DISPONÍVEL!')
