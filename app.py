542974b93b95c46c77330075a96ff29b0da5d24e
Bruno Oliveira <brunoal.flat@gmail.com>
2022-09-30 18:24:20 -0300

    Versão 2.0

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from time import sleep
 #### PASSOS ####
 
 # 1 - Entrar no site
-webbrowser.open('https://www.eventim.com.br/artist/backstreet-boys-00/backstreet-boys-curitiba-3148055/')
+webbrowser.open('https://www.eventim.com.br/artist/coldplay-0/coldplay-rio-de-janeiro-3117750/')
 sleep(3)
 
 # 2 - Dar um scroll ou pegar a barra lateral até a parte dos ingressos
@@ -20,19 +20,16 @@ teclaBar = pyautogui.locateCenterOnScreen('barLat.png')
 pyautogui.click(teclaBar[0],teclaBar[1],duration=2)
 sleep(2)
 
-#3 Descer até a parte dos ingressos (pode mudar a distância/px dependendo do monitor)
+ #3 Descer até a parte dos ingressos (pode mudar a distância/px dependendo do monitor)
 pyautogui.dragTo()
 pyautogui.scroll(-800)
 sleep(2)
 
-ingresso = pyautogui.locateCenterOnScreen('ingrDisp.png')
-while ():
-    # Verificar se a imagem é disponível ou insisponível
-    ingresso = pyautogui.locateCenterOnScreen('ingrDisp.png')
-    sleep(3)
+while True:
     # Se não estiver disponível, recomeçar com F5
-    if ingresso == True:
-        pyautogui.hotkey('F5')
-        # Se ele não estiver disponível, reiniciar.
-    elif ingresso is not True:
+    if pyautogui.locateCenterOnScreen('ingrDisp.png'):
         pyautogui.alert('TEM INGRESSO DISPONÍVEL!')
+        break
+    else:
+        pyautogui.hotkey('F5')
+        sleep(4)
